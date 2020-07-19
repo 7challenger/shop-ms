7e7ffde9c985a059bcf237252c2f56a15fd4bf04
Vadim Kurnakov <vkurnakov@spbfiller.com>
2020-07-19 06:44:00 +0300

    fix ci

--- scripts/publishImage.sh
+++ scripts/publishImage.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-# "publish:docker-api-gateway": "yarn tag:docker-api-gateway && docker push repo.treescale.com/seven-challenger/api-gateway:0.0.1",
-# "publish:docker-sites-checker": "yarn tag:docker-sites-checker && docker push repo.treescale.com/seven-challenger/sites-checker:0.0.1",
-# "publish:docker-parser": "yarn tag:docker-parser && docker push repo.treescale.com/seven-challenger/parser:0.0.1",
-
-./tagImage.sh $1 $2
+docker tag "$1:$2" "repo.treescale.com/seven-challenger/$1:$2"
 docker push "repo.treescale.com/seven-challenger/$1:$2"
--- scripts/tagImage.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# "tag:docker-api-gateway": "docker tag api-gateway:0.0.1 repo.treescale.com/seven-challenger/api-gateway:0.0.1",
-# "tag:docker-sites-checker": "docker tag sites-checker:0.0.1 repo.treescale.com/seven-challenger/sites-checker:0.0.1",
-# "tag:docker-parser": "docker tag parser:0.0.1 repo.treescale.com/seven-challenger/parser:0.0.1",
-
-docker tag "$1:$2" "repo.treescale.com/seven-challenger/$1:$2"
